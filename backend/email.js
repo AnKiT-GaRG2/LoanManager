9b277fb537395be9250a86e6af4717e8fe894907
Dhruv-Sh-git <dhruvshaswat25@gmail.com>
2025-07-26 19:01:54 +0530

    checking changes working or not

--- backend/email.js
+++ backend/email.js
@@ -9,8 +9,8 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,   // Your Gmail address
-    pass: process.env.EMAIL_PASS,   // Gmail app password
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
